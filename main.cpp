9199948ee93f5e3b3b4202ddc6e5a40c1329170e
Ren Wenhui <84836318+WenhuiRenDUT@users.noreply.github.com>
2024-07-22 21:16:13 +0800

    再次增加了for循环部分

--- main.cpp
+++ main.cpp
@@ -8,5 +8,10 @@ int main()
         x = x + i;
     }
     std::cout<< x <<std::endl;
+
+    for (int i = 0; i < 10; i++) {
+        x = x + i;
+    }
+    std::cout<< x <<std::endl;
     return 0;
 }
